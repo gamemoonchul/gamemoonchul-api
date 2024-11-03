5514f36589cec9ab04a662f85a2e33a53583d5ca
rookedsysc <rookedsysc36@gmail.com>
2024-11-04 00:42:42 +0900

    feat: commentCountCacheManager 추가

--- src/main/java/com/gamemoonchul/config/RedisCacheManagerConfig.java
+++ src/main/java/com/gamemoonchul/config/RedisCacheManagerConfig.java
@@ -1,12 +1,15 @@
 package com.gamemoonchul.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gamemoonchul.common.constants.RedisKeyConstant;
 import com.gamemoonchul.domain.entity.Post;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -23,6 +26,7 @@ import java.time.Duration;
 public class RedisCacheManagerConfig {
     private final ObjectMapper objectMapper;
 
+    @Primary
     @Bean
     public CacheManager postCacheManager(RedisConnectionFactory cf) {
         RedisSerializer<Post> valueSerializer = new Jackson2JsonRedisSerializer<>(objectMapper.getTypeFactory().constructType(Post.class));
@@ -35,4 +39,14 @@ public class RedisCacheManagerConfig {
         return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf).cacheDefaults(redisCacheConfiguration).build();
     }
 
+    @Bean
+    public CacheManager commentCountCacheManager(RedisConnectionFactory cf) {
+        RedisSerializer<Integer> valueSerializer = new Jackson2JsonRedisSerializer<>(objectMapper.getTypeFactory().constructType(Integer.class));
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer)) // Value Serializer 변경
+            .entryTtl(Duration.ofMinutes(30L)); // 캐시 수명 30분
+
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf).cacheDefaults(redisCacheConfiguration).build();
+    }
 }
