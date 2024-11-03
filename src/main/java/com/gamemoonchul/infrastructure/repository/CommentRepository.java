9bbd370455125e293212dd7703b6853de497e599
rookedsysc <rookedsysc36@gmail.com>
2024-11-04 00:26:07 +0900

    feat: CommentRepository countByPostId 추가

--- src/main/java/com/gamemoonchul/infrastructure/repository/CommentRepository.java
+++ src/main/java/com/gamemoonchul/infrastructure/repository/CommentRepository.java
@@ -1,11 +1,13 @@
 package com.gamemoonchul.infrastructure.repository;
 
+import com.gamemoonchul.common.constants.RedisKeyConstant;
 import com.gamemoonchul.domain.entity.Comment;
 import com.gamemoonchul.domain.entity.Member;
 import com.gamemoonchul.domain.entity.Post;
 import com.gamemoonchul.infrastructure.repository.ifs.CommentRepositoryIfs;
 import jakarta.persistence.LockModeType;
 import jakarta.persistence.QueryHint;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -16,6 +18,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryIfs {
+    @Cacheable(value = RedisKeyConstant.POST_COUNT_CACHEABLE, key = "#postId", cacheManager = "commentCountCacheManager")
+    Integer countByPostId(Long postId);
+
     List<Comment> findByParentId(Long parentId);
 
     List<Comment> findAllByMember(Member member);
