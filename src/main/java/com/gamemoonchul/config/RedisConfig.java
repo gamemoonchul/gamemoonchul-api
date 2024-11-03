01fd2d70ea0b6100b14480b563445bbbdb281661
rookedsysc <rookedsysc36@gmail.com>
2024-11-03 17:52:36 +0900

    feat: RedisConfig 쓸모없는 Template 지우고 String Integer template만 남겨둠

--- src/main/java/com/gamemoonchul/config/RedisConfig.java
+++ src/main/java/com/gamemoonchul/config/RedisConfig.java
@@ -1,48 +1,26 @@
 package com.gamemoonchul.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gamemoonchul.domain.entity.Member;
-import com.gamemoonchul.infrastructure.web.dto.response.PostMainPageResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.util.List;
-
 @Configuration
-@RequiredArgsConstructor
 public class RedisConfig {
-    private final ObjectMapper objectMapper;
 
     @Bean
-    RedisTemplate<String, Member> userRedisTemplate(RedisConnectionFactory connectionFactory) {
-        var template = new RedisTemplate<String, Member>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(objectMapper, Member.class));
-
-        return template;
-    }
-
-    @Bean
-    public RedisTemplate<String, List<PostMainPageResponse>> postDetailRedisTemplate(RedisConnectionFactory connectionFactory, ObjectMapper objectMapper) {
-        RedisTemplate<String, List<PostMainPageResponse>> template = new RedisTemplate<>();
+    public RedisTemplate<String, Integer> stringIntegerRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Integer> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
         // Key Serializer 설정
-        template.setKeySerializer(new GenericToStringSerializer<>(String.class));
+        template.setKeySerializer(new StringRedisSerializer());
 
         // Value Serializer 설정
-        Jackson2JsonRedisSerializer<List<PostMainPageResponse>> serializer =
-            new Jackson2JsonRedisSerializer<>(objectMapper.getTypeFactory().constructCollectionType(List.class, PostMainPageResponse.class));
-        template.setValueSerializer(serializer);
+        template.setValueSerializer(new GenericToStringSerializer<>(Integer.class));
 
-        template.afterPropertiesSet();
         return template;
     }
 }
