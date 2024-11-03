c8ed08ccaa14533fc22e66de40a7f52a42136563
rookedsysc <rookedsysc36@gmail.com>
2024-11-04 00:45:31 +0900

    feat: RedisConstant에 CommentCount Cache Key 추가

--- src/main/java/com/gamemoonchul/common/constants/RedisKeyConstant.java
+++ src/main/java/com/gamemoonchul/common/constants/RedisKeyConstant.java
@@ -1,7 +1,13 @@
 package com.gamemoonchul.common.constants;
 
 public class RedisKeyConstant {
+    /**
+     * Cacheable에서 사용하는 키
+     */
+    public static final String POST_COUNT_CACHEABLE = "post:count";
     public static String postCountKey(Long postId) {
-        return "post:count:" + postId;
+        // Cacheable에서 자동으로 뒤에 ::을 붙이기 때문에
+        // Sync를 맞출려면 붙여줘야 함
+        return POST_COUNT_CACHEABLE + "::" + postId;
     }
 }
