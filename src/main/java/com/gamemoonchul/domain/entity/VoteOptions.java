ba870fddf22c879f4453ff1e175744cb376f5141
lokigem <holmes0306@naver.com>
2024-12-13 12:46:48 +0900

    feat: VoteOptions Vote 연관 BatchSize 활용해서 N + 1 최적화

--- src/main/java/com/gamemoonchul/domain/entity/VoteOptions.java
+++ src/main/java/com/gamemoonchul/domain/entity/VoteOptions.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.gamemoonchul.domain.entity.riot.MatchUser;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class VoteOptions {
+    @BatchSize(size = 50)
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "voteOption", cascade = CascadeType.ALL, orphanRemoval = true)
     final private List<Vote> votes = new ArrayList<>();
     @Id
@@ -25,7 +27,7 @@ public class VoteOptions {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "match_user_id")
     private MatchUser matchUser;
 
